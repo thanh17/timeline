3345e4a587b5baa3f54374852187edd309d5b76b
Thanh Nguyen <thanhn1756@gmail.com>
2020-12-18 17:20:07 -0800

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Thanh Nguyen on 12/18/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/timelineApp.swift
@@ -0,0 +1,17 @@
+//
+//  timelineApp.swift
+//  Shared
+//
+//  Created by Thanh Nguyen on 12/18/20.
+//
+
+import SwiftUI
+
+@main
+struct timelineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
